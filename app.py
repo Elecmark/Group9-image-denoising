2601b1a1a8b61fdfa4f83c358186289b43cb60bc
PDH <944723112@qq.com>
2023-11-20 20:31:32 +0800

    将html放入app.py中

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+def home():  # 更改函数名称以更好地反映其功能
+    return render_template('Denoising.html')  # 使用 render_template 来加载 HTML 文件
 
 if __name__ == '__main__':
     app.run()
