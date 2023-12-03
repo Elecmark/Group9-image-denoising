e2d248708a7adb5ac653a8ab02251404a794c77f
Yee334 <2418748180@qq.com>
2023-12-03 14:03:24 +0800

    修改

--- app.py
+++ app.py
@@ -1,26 +1,48 @@
-from flask import Blueprint,request, jsonify, render_template, json,session, redirect, flash,url_for,views,g
-from app.utils import restful
-from app.utils.upload import change_filename
-from werkzeug.utils import secure_filename
-
+from flask import Flask, render_template, request
 import os
 
-bp = Blueprint('test',__name__)
+app = Flask(__name__)
 
-@bp.route('/test/upload/',methods=['POST','GET'])
-# @login_required
-def upload():
-    if request.method == 'POST':
-        # 定义上传目录，如果目录不存在，则自动创建
-        UPLOAD_FOLDER = os.getcwd() + '/app/static/upload/'    # 上传后保存的本地路径
-        file_dir = os.path.join(os.getcwd(), UPLOAD_FOLDER)#constants.
-        if not os.path.exists(file_dir):
-            os.makedirs(file_dir)
+# 获取当前文件（app.py）的绝对路径
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
-        image = request.files['file']  # 获取前端提交过来的图片
-        filename = secure_filename(change_filename(image.filename))  # 修改图片上传的图片名称
-        file_path = os.path.join(UPLOAD_FOLDER, filename)  # 获取上传后的绝对路径
-        image.save(file_path)  # 保存到本地
-        return restful.success(data=os.path.join('/static/upload', filename))
-    return render_template('test/upload.html')
+# 设置上传文件存储的文件夹为'images'的相对路径
+UPLOAD_FOLDER = os.path.join('images')
+# 允许的文件扩展名
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def home():
+    return render_template('Denoising.html')
+
+@app.route('/upload', methods=['POST', 'GET'])
+def upload_file():
+    if 'image' not in request.files:
+        return '没有文件部分'
+
+    image = request.files['image']
+
+    if image.filename == '':
+        return '未选择文件'
+
+    if image and allowed_file(image.filename):
+        # 移除之前的 'Original' 图片
+        previous_image_path = os.path.join(app.config['UPLOAD_FOLDER'], 'Original.jpg')
+        if os.path.exists(previous_image_path):
+            os.remove(previous_image_path)
+
+        # 保存上传的图片，文件名为 'Original'，保留原始格式
+        save_path = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], 'Original' + os.path.splitext(image.filename)[1])
+        image.save(save_path)
+
+        return '图片上传成功！'
+    else:
+        return '无效的文件格式。支持的格式：png、jpg、jpeg、gif'
+
+if __name__ == '__main__':
+    app.run()
